598c99e1c22b42c386baec41621d3423ebb96aea
Cristian Sánchez <harmaharcri@hotmail.com>
2023-06-07 17:24:10 -0500

    Improved source code

--- domain/src/main/java/co/crisi/productmanagement/domain/CommonProduct.java
+++ domain/src/main/java/co/crisi/productmanagement/domain/CommonProduct.java
@@ -2,13 +2,19 @@ package co.crisi.productmanagement.domain;
 
 import java.util.Objects;
 
+import static co.crisi.productmanagement.domain.validators.Validations.nonNull;
+import static co.crisi.productmanagement.domain.validators.Validations.nonEmpty;
+
 public record CommonProduct(String id, String name, String description, double price) implements IProduct {
 
 
     private static final int MINIMUM_NAME_CHARACTER_LENGTH = 5;
 
-    public CommonProduct {
-        isNameValid();
+    public CommonProduct(String id, String name, String description, double price) {
+        this.id = nonEmpty(id, "The id should not be empty");
+        this.name = nonEmpty(name, "The name should not be empty");
+        this.description = nonEmpty(description, "The description should not be empty");
+        this.price = nonNull(price, "The price should not be empty");
     }
 
     @Override
