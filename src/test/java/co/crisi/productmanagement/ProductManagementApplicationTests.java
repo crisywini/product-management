e900529d390e9b3be30d1e8ac80a7aa86f7bea5c
Cristian Sánchez <harmaharcri@hotmail.com>
2023-06-07 15:09:33 -0500

    Init project

---
+++ src/test/java/co/crisi/productmanagement/ProductManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package co.crisi.productmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
