f76cd40fc17ce9f5f746e91964d7a85e45b23606
Cristian Sánchez <harmaharcri@hotmail.com>
2023-06-15 16:43:38 -0500

    Fixed the spring context

--- adapter/src/main/java/co/crisi/productmanagement/App.java
+++ adapter/src/main/java/co/crisi/productmanagement/App.java
@@ -1,4 +1,4 @@
-package co.crisi.productmanagement.start;
+package co.crisi.productmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
